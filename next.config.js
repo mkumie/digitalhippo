e78155c945de4cd13ba11cdf85f0e07c734fcc4c
Moses <moses.kumie@outlook.com>
2024-01-16 17:57:18 +1000

    changed to domains for image urls

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-        pathname: "**",
-        port: "3001",
-        protocol: "http",
-      },
-      {
-        hostname: "digitalhippo-two.vercel.app",
-        pathname: "**",
-        protocol: "https",
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     hostname: "localhost",
+    //     pathname: "**",
+    //     port: "3001",
+    //     protocol: "http",
+    //   },
+    //   {
+    //     hostname: "digitalhippo-two.vercel.app",
+    //     pathname: "**",
+    //     protocol: "https",
+    //   },
+    // ],
+    domains: ["localhost", "digitalhippo-two.vercel.app"],
   },
 };
 
